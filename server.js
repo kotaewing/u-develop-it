04a78f22fc8ce0a52d3360ec868f5b4d5b23e518
kotaewing <kota.ewing@gmail.com>
2021-05-24 16:08:27 -0600

    successfully connected to mysql server

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'amberglen7',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 
 
